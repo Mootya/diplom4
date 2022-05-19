9dc2fbf79d4a37f061dd25b86a543d47def8c5b4
Mootya <67216394+Mootya@users.noreply.github.com>
2022-05-19 10:58:44 +0300

    Add files via upload

---
+++ KURS1/KURS1/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+
+namespace KURS1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+           
+         
+        }
+
+        private void voyti_Click(object sender, RoutedEventArgs e)
+        {
+            {
+                try
+                {
+                    using (SqlConnection connection = new SqlConnection(connectionString))
+                    {
+                        connection.Open();
+                        SqlCommand command = new SqlCommand("select *  from [avtoriz] where [avtoriz].[login] = '"
+                            + login.Text + "'and [avtoriz].[Password] = '" + password.Text + "'", connection);
+                        SqlDataReader reader = command.ExecuteReader();
+                        if (reader.HasRows)//если естьданные 
+                        {
+                            while (reader.Read())// построчно считываем данные
+                            {
+                                string rol = reader.GetValue(3).ToString();
+                                MessageBox.Show("Добро пожаловать!");
+
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("Такого пользователя нет");
+                        }
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("Введите корректные данные");
+                }
+            }
+        }
+
+        private void reg_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 form1 = new Window1();
+            form1.Show();
+            Hide();
+        }
+    }
+}
---
+++ KURS1/KURS1/Window1.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace KURS1
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                //проверка пароля
+                string s = Password.Text;
+                char[] array = s.ToCharArray(); // раскладываем строку парля на знаки
+                int d = s.Length;
+                int k = 0;
+                int u = 0;
+                int b = 0;
+                char p = '$';
+                char j = '!';
+                char f = '@';
+                char h = '%';
+                char z = '^';
+                char x = '#';
+
+                // проверка на Верхний регистр
+                for (int i = 0; i < d; i++)
+                {
+                    if (char.IsUpper(array[i]))//вычисляем регистр
+                        k++;
+                }
+
+                // проверка на число
+                for (int i = 0; i < d; i++)
+                {
+                    if (char.IsNumber(array[i]))//вычисляем числа
+                        u++;
+                }
+
+                // проверка на знак
+                for (int i = 0; i < d; i++)
+                {//вычисляем знак
+                    if (Convert.ToChar(p) == (array[i]) || Convert.ToChar(j) ==
+                    (array[i]) || Convert.ToChar(h) == (array[i]) || Convert.ToChar(z) == (array[i]) ||
+                    Convert.ToChar(f) == (array[i]) || Convert.ToChar(x) == (array[i]))
+                        b++;
+                }
+
+
+
+                if ((k >= 1) && (Password.Text.Length >= 6) && (u >= 1) && (b >= 1))
+                {
+                    using (SqlConnection connection = new SqlConnection(connectionString))
+                    {
+                        connection.Open();
+                        SqlCommand command = new SqlCommand("insert into [avtoriz] ([avtoriz].[login], [avtoriz].[Password],[avtoriz].[Roly] ,[avtoriz]. [FIO]) values ('" + login.Text + "','" + Password.Text + "','" + FIO.Text + "')", connection);
+                        SqlDataReader reader = command.ExecuteReader();
+                        MessageBox.Show("Пользователь добавлен");
+                        Close();
+                    }
+
+                }
+                else
+                {
+                    MessageBox.Show("Пароль должен содержать $ ! @ # ^ %, как минимум 1 цифру, как минимум одну заглавную букву");
+                }
+            }
+
+            catch
+            {
+                MessageBox.Show("Введите корректные данные");
+            }
+        }
+    }
+}
